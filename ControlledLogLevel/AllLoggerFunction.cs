3d062eda27c4abb8038949422846f2a3bc38f175
ykgenensis <yogesh.kadalgikar@genesis-fs.com>
2022-05-02 12:31:13 +0530

    Add project files.

---
+++ ControlledLogLevel/AllLoggerFunction.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ControlledLogLevel
+{
+    internal class AllLoggerFunction
+    {
+        public AllLoggerFunction(ILogger<AllLoggerFunction> logger)
+        {
+            _Logger = logger;
+        }
+
+        public ILogger<AllLoggerFunction> _Logger { get; }
+
+        [FunctionName("GetAllLogs")]
+        public void GetAllLogs(
+          [HttpTrigger(AuthorizationLevel.Anonymous, "get")]
+          HttpRequest req)
+        {
+            _Logger.LogTrace("Trace Logged");
+            _Logger.LogDebug("Debug Logged");
+            _Logger.LogInformation("Information Logged");
+            _Logger.LogWarning("Warning Logged");
+            _Logger.LogError("Error Logged");
+            _Logger.LogCritical("Fatal Logged");
+        }
+    }
+}
---
+++ ControlledLogLevel/StartUp.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection;
+using Serilog;
+using Serilog.Core;
+using Serilog.Events;
+
+[assembly: FunctionsStartup(typeof(ControlledLogLevel.StartUp))]
+
+namespace ControlledLogLevel
+{
+    
+    public class StartUp : FunctionsStartup
+    {
+        public static LoggingLevelSwitch LogLevelSwitch { get; private set; }
+        
+        public override void Configure(IFunctionsHostBuilder builder)
+        {
+            LogLevelSwitch = new LoggingLevelSwitch(LogEventLevel.Verbose);
+
+            builder.Services.AddLogging(configure =>
+            {  
+                Log.Logger = new LoggerConfiguration()
+                    .MinimumLevel.ControlledBy(LogLevelSwitch)
+                    .WriteTo.Console()
+                    .CreateLogger();
+
+                configure.AddSerilog(Log.Logger);
+            });
+        }
+    }
+}
